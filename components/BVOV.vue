8db9a6cda63032eb3c1ee1646b776420524dc7d2
Olivia Meng <yxm011@kcmhq.org>
2024-07-30 17:11:58 -0500

    updatedthe layput of  components

--- components/BVOV.vue
+++ components/BVOV.vue
@@ -1,9 +1,15 @@
 <template>
 
-    <div>
+    <div class="component-box">
+
          2. BVOV
+
         
+
+
+   
     </div>
+
 </template>
 
 
@@ -12,5 +18,359 @@
 </script>
 
 <style>
+.title {
+  align-items: flex-start;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  height: 864.22px;
+  margin-bottom: -100.22px;
+  position: relative;
+  width: 736px;
+}
+
+.title .frame-4 {
+  align-items: flex-start;
+  align-self: stretch;
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: column;
+  gap: 12px;
+  position: relative;
+  width: 100%;
+}
+
+
+
+.title .text-wrapper-2 {
+  color: #000000;
+  font-family: var(--heading-h1-font-family);
+  font-size: var(--heading-h1-font-size);
+  font-style: var(--heading-h1-font-style);
+  font-weight: var(--heading-h1-font-weight);
+  letter-spacing: var(--heading-h1-letter-spacing);
+  line-height: var(--heading-h1-line-height);
+  margin-top: -1px;
+  position: relative;
+  width: 454px;
+}
+
+.title .p {
+  color: #000000;
+  font-family: var(--heading-h2-font-family);
+  font-size: var(--heading-h2-font-size);
+  font-style: var(--heading-h2-font-style);
+  font-weight: var(--heading-h2-font-weight);
+  letter-spacing: var(--heading-h2-letter-spacing);
+  line-height: var(--heading-h2-line-height);
+  margin-right: -94px;
+  position: relative;
+  width: 830px;
+}
+
+.title .frame-5 {
+  align-items: flex-start;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 24px;
+  position: relative;
+}
+
+.title .content-2 {
+  align-items: flex-start;
+  display: flex;
+  gap: 16px;
+  padding: 0px 0px 24px;
+  position: relative;
+  width: 355px;
+}
+
+.title .frame-6 {
+  align-items: flex-start;
+  display: inline-flex;
+  flex: 0 0 auto;
+  flex-direction: column;
+  gap: 16px;
+  margin-right: -1px;
+  position: relative;
+}
+
+.title .mask-group {
+  height: 199.61px;
+  position: relative;
+  width: 355px;
+}
+
+.title .frame-7 {
+  align-items: flex-start;
+  display: inline-flex;
+  flex: 0 0 auto;
+  flex-direction: column;
+  gap: 16px;
+  position: relative;
+}
+
+.title .text-wrapper-3 {
+  color: #000000;
+  font-family: var(--heading-h5-font-family);
+  font-size: var(--heading-h5-font-size);
+  font-style: var(--heading-h5-font-style);
+  font-weight: var(--heading-h5-font-weight);
+  letter-spacing: var(--heading-h5-letter-spacing);
+  line-height: var(--heading-h5-line-height);
+  margin-top: -1px;
+  position: relative;
+  width: 356px;
+}
+
+.title .text-wrapper-4 {
+  color: #000000;
+  font-family: var(--body-small-font-family);
+  font-size: var(--body-small-font-size);
+  font-style: var(--body-small-font-style);
+  font-weight: var(--body-small-font-weight);
+  letter-spacing: var(--body-small-letter-spacing);
+  line-height: var(--body-small-line-height);
+  position: relative;
+  width: 356px;
+}
+
+.title .content-3 {
+  align-items: flex-start;
+  display: flex;
+  justify-content: space-around;
+  position: relative;
+  width: 355px;
+}
+
+.title .frame-8 {
+  align-items: flex-start;
+  display: inline-flex;
+  flex: 0 0 auto;
+  flex-direction: column;
+  gap: 16px;
+  height: 320px;
+  position: relative;
+}
+
+.title .group {
+  height: 68px;
+  margin-right: -4px;
+  position: relative;
+  width: 359px;
+}
+
+.title .img {
+  height: 62px;
+  left: 0;
+  position: absolute;
+  top: 4px;
+  width: 110px;
+}
+
+.title .text-wrapper-5 {
+  color: #000000;
+  font-family: var(--body-small-font-family);
+  font-size: var(--body-small-font-size);
+  font-style: var(--body-small-font-style);
+  font-weight: var(--body-small-font-weight);
+  left: 125px;
+  letter-spacing: var(--body-small-letter-spacing);
+  line-height: var(--body-small-line-height);
+  position: absolute;
+  top: 47px;
+  width: 230px;
+}
+
+.title .text-wrapper-6 {
+  color: #000000;
+  font-family: var(--heading-h6-font-family);
+  font-size: var(--heading-h6-font-size);
+  font-style: var(--heading-h6-font-style);
+  font-weight: var(--heading-h6-font-weight);
+  left: 125px;
+  letter-spacing: var(--heading-h6-letter-spacing);
+  line-height: var(--heading-h6-line-height);
+  position: absolute;
+  top: 0;
+  width: 230px;
+}
+
+.title .secondary-CTA-wrapper {
+  align-items: flex-start;
+  display: inline-flex;
+  gap: 24px;
+  height: 48px;
+  margin-bottom: -64px;
+  padding: 12px 0px 0px;
+  position: relative;
+}
+
+.title .secondary-CTA {
+  align-items: center;
+  border: 1px solid;
+  border-color: #21252933;
+  border-radius: 4.8px;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 8px;
+  height: 48px;
+  justify-content: center;
+  margin-bottom: -12px;
+  overflow: hidden;
+  padding: 8px 16px;
+  position: relative;
+}
+
+.title .text-text-instance {
+  background-color: #ffffff00 !important;
+  flex: 0 0 auto !important;
+}
+
+.title .design-component-instance-node {
+  font-family: var(--heading-h5-font-family) !important;
+  font-size: var(--heading-h5-font-size) !important;
+  font-style: var(--heading-h5-font-style) !important;
+  font-weight: var(--heading-h5-font-weight) !important;
+  letter-spacing: var(--heading-h5-letter-spacing) !important;
+  line-height: var(--heading-h5-line-height) !important;
+}
+
+.title .TV-offer {
+  align-self: stretch;
+  height: 200.39px;
+  position: relative;
+  width: 100%;
+}
+
+.title .mobile {
+  height: 208px;
+  position: relative;
+  top: 8px;
+  width: 736px;
+}
+
+.title .overlap-wrapper {
+  height: 208px;
+  width: 742px;
+}
+
+.title .overlap {
+  height: 215px;
+  position: relative;
+  width: 736px;
+}
+
+.title .overlap-group {
+  height: 212px;
+  left: 0;
+  position: absolute;
+  top: 3px;
+  width: 736px;
+}
+
+.title .background {
+  height: 154px;
+  left: 0;
+  position: absolute;
+  top: 35px;
+  width: 736px;
+}
+
+.title .button {
+  all: unset;
+  align-items: center;
+  background-color: #ffffff;
+  border: 1px solid;
+  border-radius: 200px;
+  box-shadow: -3px 3px 4px #003f6040;
+  box-sizing: border-box;
+  display: flex;
+  gap: 12px;
+  height: 38px;
+  justify-content: center;
+  left: 473px;
+  padding: 10px 20px;
+  position: absolute;
+  top: 123px;
+  width: 225px;
+}
+
+.title .text-2 {
+  align-items: center;
+  background-color: #ffffff;
+  display: inline-flex;
+  flex: 0 0 auto;
+  gap: 12px;
+  margin-bottom: -3px;
+  margin-top: -3px;
+  position: relative;
+}
+
+.title .text-3 {
+  color: var(--neutral-dark);
+  font-family: "Inter", Helvetica;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0;
+  line-height: 24px;
+  margin-top: -1px;
+  position: relative;
+  white-space: nowrap;
+  width: fit-content;
+}
+
+.title .text-wrapper-7 {
+  color: #ffffff;
+  font-family: "Poppins", Helvetica;
+  font-size: 20px;
+  font-weight: 500;
+  left: 198px;
+  letter-spacing: 0;
+  line-height: 24px;
+  position: absolute;
+  text-align: right;
+  top: 85px;
+  white-space: nowrap;
+  width: 502px;
+}
+
+.title .text-wrapper-8 {
+  color: #ffffff;
+  font-family: "Poppins", Helvetica;
+  font-size: 18px;
+  font-weight: 700;
+  left: 144px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: right;
+  top: 58px;
+  width: 556px;
+}
+
+.title .image {
+  height: 212px;
+  left: 41px;
+  object-fit: cover;
+  position: absolute;
+  top: 0;
+  width: 154px;
+}
+
+.title .text-wrapper-9 {
+  color: #404040;
+  font-family: "Poppins", Helvetica;
+  font-size: 24px;
+  font-weight: 700;
+  height: 33px;
+  left: 208px;
+  letter-spacing: 0;
+  line-height: normal;
+  position: absolute;
+  text-align: right;
+  top: 0;
+  width: 490px;
+}
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,126 +1,12 @@
-<!-- <template>
-  <div class="container">
-
-
-    <section id="Page-Header" class="component">
-      <PageHeader />
-    </section>
-    
-    <section id="BVOV" class="component large">
-      <BVOV />
-    </section>
-
-    <section id="Live-TV" class="component small">
-      <LiveTV />
-    </section>
-
-    <section id="ITV-Video" class="component small">
-      <InsideTheVisionVideo />
-    </section>
-
-    <section id="Free-TV" class="component">
-      <FreeTVOffer />
-    </section>
-
-    <section id="Recent-Videos" class="component">
-      <RecentVideos />
-    </section>
-
-    <section id="Popular-Videos" class="component">
-      <PopularVideos />
-    </section>
-
-    <section id="Faith-Foundations" class="component">
-      <FaithFoundations />
-    </section>
-
-    <section id="Sow-Seeds" class="component">
-      <SowSeedsOfChange />
-    </section>
-
-    <section id="Events-On-Demand" class="component">
-      <EventsOnDemand />
-    </section>
-
-    <section id="Inside-the-Vision" class="component">
-      <InsideTheVision />
-    </section>
-
-    <section id="Search-Programs" class="component">
-      <SearchPrograms />
-    </section>
-
-    <section id="Ways-to-Watch" class="component">
-      <WaysToWatch />
-    </section>
-
-    <section id="Give-Prayer-Partner" class="component">
-      <CTA />
-    </section>
-
-    <section id="Page-Footer" class="component">
-      <PageFooter />
-    </section>
-  </div>
-</template>
-
-<script setup>
-
-</script>
-
-
-<style scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.component {
-  border: 2px solid #800080;
-  min-height: 100px;
-}
-
-.large {
-  min-height: 300px;
-}
-
-.small {
-  min-height: 150px;
-}
-
-@media (min-width: 768px) {
-  .container {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-
-  #BVOV, #Free-TV, #Recent-Videos, #Popular-Videos, #Faith-Foundations, #Sow-Seeds, #Events-On-Demand, #Inside-the-Vision, #Search-Programs, #Ways-to-Watch, #Give-Prayer-Partner, #Page-Footer {
-    flex: 1 1 100%;
-  }
-
-  #Live-TV, #ITV-Video {
-    flex: 1 1 50%;
-  }
-}
-
-@media (min-width: 1024px) {
-  #BVOV {
-    flex: 2 1 60%;
-  }
-
-  #Live-TV, #ITV-Video {
-    flex: 1 1 20%;
-  }
-}
-</style> -->
-
 <template>
+  <div class="wrapper">
+
+  
   <div class="container">
     <section id="Page-Header" class="component">
       <PageHeader />
     </section>
-    
+
     <section id="BVOV" class="component">
       <BVOV />
     </section>
@@ -161,7 +47,7 @@
       <InsideTheVision />
     </section>
 
-    <section id="Search-Programs" class="component">
+    <section id="Search-Programs" class="component small-height">
       <SearchPrograms />
     </section>
 
@@ -177,38 +63,198 @@
       <PageFooter />
     </section>
   </div>
+
+</div>
+
 </template>
 
 <script setup>
-
 </script>
 
 <style scoped>
+
+.wrapper{
+  max-width: 390px;
+  margin: 0 auto;
+}
+
 .container {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  /* gap: 40px; */
 }
 
 .component {
   border: 2px solid #800080;
-  min-height: 100px;
+ 
 }
 
+
+
+#Page-Header{
+  height: 92px;
+  margin-bottom: 24px;
+
+}
+
+#BVOV{
+
+  height: 1024px;
+  margin-bottom: 36px;
+
+
+}
+
+#Live-TV{
+
+  height: 234px;
+  margin-bottom: 36px;
+  
+}
+
+#ITV-Video{
+  height: 263px;
+  margin-bottom: 36px;
+
+}
+
+#Free-TV{
+
+  height: 313px;
+  margin-bottom: 36px;
+}
+
+
+#Recent-Videos{
+
+  height: 291px;
+  margin-bottom: 36px;
+}
+
+#Popular-Videos{
+  height: 291px;
+  margin-bottom: 36px;
+}
+
+
+#Faith-Foundations{
+  height: 305px;
+  margin-bottom: 36px;
+}
+
+
+#Sow-Seeds{
+  height: 304px;
+  margin-bottom: 36px;
+}
+
+#Events-On-Demand{
+
+  padding-top: 24px;
+  height: 1575px;
+  margin-bottom: 36px;
+  
+}
+
+
+
+#Inside-the-Vision{
+  padding-top: 36px;
+  padding-bottom: 36px;
+  height: 975px;
+  margin-bottom: 36px;
+}
+
+#Search-Programs{
+
+  height: 98px;
+  margin-bottom: 36px;
+
+}
+
+#Ways-to-Watch{
+  height: 1240px;
+  margin-bottom: 36px;
+}
+
+
+
+#Give-Prayer-Partner{
+
+ 
+  height: 759px;
+    margin-bottom: 72px;
+
+}
+
+
+
+#Page-Footer{
+  height: 1287px;
+}
+
+
+
+
+/* 3 rows * 100px + 2 gaps */
+/* #Events-On-Demand {
+  height: calc(5 * 100px + 20px); 
+}
+
+#Inside-the-Vision {
+  height: calc(4 * 100px + 10px); 
+}
+
+#Ways-to-Watch {
+  height: calc(7 * 100px + 30px);
+}
+
+#Page-Footer {
+  height: calc(2 * 100px + 10px); 
+} */
+
+/* #BVOV {
+  height: calc(4 * 100px + 10px); 
+} */
+
+
+
 @media (min-width: 768px) {
+
+  .wrapper{
+    max-width: 1280px;
+    margin: 0 auto;
+    
+  }
+
   .container {
     display: grid;
     grid-template-columns: 2fr 1fr;
-    gap: 10px;
+    /* gap: 40px; */
+    grid-auto-rows: minmax(100px, auto); /* Ensure rows can expand as needed */
   }
 
-  #Page-Header {
+
+  #Free-TV, #Recent-Videos, #Popular-Videos, #Faith-Foundations, #Sow-Seeds,  #Ways-to-Watch, #Give-Prayer-Partner, #Page-Footer {
     grid-column: 1 / -1;
   }
+
+  
+  #Page-Header{
+
+    height: 163px;
+    margin-bottom: 60px;
+    grid-column: 1 / -1;
+  
+  }
+
 
   #BVOV {
     grid-column: 1 / 2;
     grid-row: 2 / 4;
+    height: 541px;
+    margin-right: 24px ;
+
   }
 
   #Live-TV, #ITV-Video {
@@ -217,14 +263,91 @@
 
   #Live-TV {
     grid-row: 2 / 3;
+    
+    
   }
 
   #ITV-Video {
     grid-row: 3 / 4;
   }
 
-  #Free-TV, #Recent-Videos, #Popular-Videos, #Faith-Foundations, #Sow-Seeds, #Events-On-Demand, #Inside-the-Vision, #Search-Programs, #Ways-to-Watch, #Give-Prayer-Partner, #Page-Footer {
-    grid-column: 1 / -1;
+  #Free-TV{
+    height: 157px;
+    margin-bottom: 60px;
   }
+
+  #Recent-Videos{
+
+    height: 272px;
+    margin-bottom: 60px;
+  }
+
+  #Popular-Videos{
+    height: 272px;
+    margin-bottom: 60px;
+
+  }
+
+
+#Faith-Foundations{
+  height: 282px;
+  margin-bottom: 60px;
+}
+
+#Sow-Seeds{
+
+  height: 166px;
+  margin-bottom: 60px;
+}
+
+
+#Events-On-Demand{
+
+  grid-column: 1 / -1;
+  padding-top: 24px;
+
+  height: 714px;
+  margin-bottom: 60px;
+}
+
+  #Inside-the-Vision {
+    grid-column: 1 / -1;
+    height: 589px;
+    margin-bottom: 60px;
+  }
+
+
+
+  #Search-Programs {
+    grid-column: 1 / -1;
+  
+    height: 98px;
+    margin-bottom: 60px;
+  }
+
+
+  #Ways-to-Watch {
+    grid-column: 1 / -1;
+    height: 908px;
+    margin-bottom: 60px;
+
+   
+  }
+
+  #Give-Prayer-Partner{
+
+    height: 205px;
+    margin-bottom: 60px;
+
+  }
+
+  #Page-Footer {
+    grid-column: 1 / -1;
+    height: 653px;
+  }
+
+
+
+
 }
 </style>
